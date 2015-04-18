9dda0e0e5fd46e5223027e422365c622da0d7757
michaelbiby <michaelbiby619@gmail.com>
2015-04-18 14:10:06 -0400

    sql file added named as query.sql

---
+++ src/query.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE words (serial_no INTEGER (5), word VARCHAR (10));
+
+
+INSERT INTO words VALUES (1,'Rimal');
+INSERT INTO words VALUES (2,'Mark');
+INSERT INTO words VALUES (3,'Raj');
+INSERT INTO words VALUES (4,'Len');
+INSERT INTO words VALUES (5,'Jim');
+INSERT INTO words VALUES (6,'Eric');
+INSERT INTO words VALUES (7,'Pavlo');
+INSERT INTO words VALUES (8,'Venu');
+INSERT INTO words VALUES (9,'Sunil');
+INSERT INTO words VALUES (10,'Rick');
+INSERT INTO words VALUES (11,'Madhuri');
+INSERT INTO words VALUES (12,'Neha');
+INSERT INTO words VALUES (13,'Bino');
+INSERT INTO words VALUES (14,'Michael');
+INSERT INTO words VALUES (15,'Kuldip');
+INSERT INTO words VALUES (16,'Stone');
+INSERT INTO words VALUES (17,'Revathi');
+INSERT INTO words VALUES (18,'Vicky');
+INSERT INTO words VALUES (19,'Ashley');
+INSERT INTO words VALUES (20,'Payne');
+
+
+select * from words;
